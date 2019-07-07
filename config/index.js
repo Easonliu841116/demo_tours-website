806539c790a4c6589b75b3c7099729032cbcc265
Eason Liu <a0956031116@gmail.com>
2019-07-07 18:37:52 +0800

    build

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/demo-build/',
+    assetsPublicPath: '/demo_tours-website/dist/',
 
     /**
      * Source Maps
